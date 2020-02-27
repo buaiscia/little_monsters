9a286e4af0b14767b0edf54f515fbd204033d19f
Alex Buaiscia (HCI) <alex.buaiscia@gmail.com>
2020-02-27 19:19:21 +0100

    few aesthetic fixes

--- src/components/card-list/card-list.jsx
+++ src/components/card-list/card-list.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Card } from '../card/card';
 import './card-list.styles.css'
 
-export const CardList = (props) => {
+export const CardList = props => {
     return (
         <div className='card-list'>
             {props.monsters.map(monster => (
--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './card.style.css'
 
 
-export const Card = (props) => (
+export const Card = props => (
     <div className='card-container'>
         <img alt='monster' src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}/>
         <h2> {props.monster.name} </h2>
